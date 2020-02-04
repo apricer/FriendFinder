532f838a81349bd136b0df09210d5d67a2df3329
Anthony Pricer <anthony.pricer@outlook.com>
2020-02-04 17:43:32 -0600

    FriendFinder

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,7 +1,7 @@
 var friends = [
     {
-        "name": "Ahmed",
-        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "name": "Pat",
+        "photo": "https://www.google.com/imgres?imgurl=https%3A%2F%2Fupload.wikimedia.org%2Fwikipedia%2Fen%2F9%2F99%2FItispat.jpg&imgrefurl=https%3A%2F%2Fen.wikipedia.org%2Fwiki%2FPat_(Saturday_Night_Live)&tbnid=J76Ea4mZXdB2nM&vet=12ahUKEwi_pMDFkrfnAhUF3KwKHQqkB_MQMygAegUIARDcAQ..i&docid=jQSu6AJQKMgIdM&w=220&h=300&q=pat%20snl&hl=en&ved=2ahUKEwi_pMDFkrfnAhUF3KwKHQqkB_MQMygAegUIARDcAQ://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
         "scores": [
             5,
             1,
--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -24,3 +24,30 @@ module.exports = function (app) {
         res.sendFile(path.join(__dirname, "../public/home.html"));
     });
 };
+// ===============================================================================
+// DEPENDENCIES
+// We need to include the path package to get the correct file path for our html
+// ===============================================================================
+var path = require("path");
+
+// ===============================================================================
+// ROUTING
+// ===============================================================================
+
+module.exports = function (app) {
+
+    // HTML GET Requests
+    // Below code handles when users "visit" a page.
+    // In each of the below cases the user is shown an HTML page of content
+    // ---------------------------------------------------------------------------
+
+    app.get("/survey", function (req, res) {
+        res.sendFile(path.join(__dirname, "/../public/survey.html"));
+    });
+
+    // If no matching route is found default to home
+    app.get("*", function (req, res) {
+        res.sendFile(path.join(__dirname, "/../public/home.html"));
+    });
+
+};
